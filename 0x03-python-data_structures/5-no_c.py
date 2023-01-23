56f918bb5fd8252f9d94891f2e53d75ef9eb5b3b
Leykun <leykungizaw@gmail.com>
2023-01-23 15:16:42 +0000

    Task 5 of 0x03-python-data_structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+    for elem in my_string:
+        if elem == "c" or elem == "C":
+            continue
+        new_string += elem
+    return new_string
