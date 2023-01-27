a8f946ae813e75d9be3f67c0362c268803b783ce
Leykun <leykungizaw@gmail.com>
2023-01-27 07:49:52 +0000

    Task 9 of 0x03-python-data_structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def multiple_returns(sentence: str)-> tuple:
+def multiple_returns(sentence: str) -> tuple:
     if sentence:
         return (len(sentence), sentence[0])
     return (0, None)
