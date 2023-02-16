730efa96eeef98ec39e4b765de6a2c554e372352
Leykun <leykungizaw@gmail.com>
2023-02-16 15:08:33 +0000

    Task 1 of 0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Constructor method for Square method
+
+        Args:
+            self (Square): Referring to current class
+            size (int): Size of Square
+        """
+        self.__size = size
+        return
+    pass
