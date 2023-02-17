88513f20208ab30e3603d1bdd83fbb7931be6c37
Leykun <leykungizaw@gmail.com>
2023-02-17 07:08:12 +0000

    Task 5 of 0x06-python-classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module defines Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Constructor method for Square method
+
+        Args:
+            self (Square): Referring to current class
+            size (int): Size of Square
+        """
+        self.size = size
+        return
+
+    @property
+    def size(self):
+        """The size property getter."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size property
+
+        Args:
+            size (int): Size of square
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Print square to terminal"""
+        if self.size:
+            for _ in range(self.size):
+                print("#" * self.size)
+        else:
+            print()
+    pass
