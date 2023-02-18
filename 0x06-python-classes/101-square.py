469808386c097f2d61a7c0615b0aa96fb9a3ff3b
Leykun <leykungizaw@gmail.com>
2023-02-18 07:25:37 +0000

    Task 8 of 0x06-python-classes

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""Module defines Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Constructor method for Square method
+
+        Args:
+            self (Square): Referring to current class
+            size (int): Size of Square
+        """
+        self.size = size
+        self.position = position
+        return
+
+    @property
+    def size(self):
+        """The size property getter."""
+        return self.__size
+
+    @size.setter
+    def size(self, sz: int):
+        """Set the size property
+
+        Args:
+            size (int): Size of square
+        """
+        if type(sz) is not int:
+            raise TypeError("size must be an integer")
+        if sz < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = sz
+        return
+
+    @property
+    def position(self):
+        """The position property getter."""
+        return self.__position
+
+    @position.setter
+    def position(self, pos):
+        """Set the position property
+
+        Args:
+            size (int): Size of square
+        """
+        if type(pos) == tuple and len(pos) == 2 and type(pos[0]) == int and \
+           type(pos[1]) == int and pos[0] >= 0 and pos[1] >= 0:
+            self.__position = pos
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Print square to terminal"""
+        if self.size:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)
+        else:
+            print()
+
+    def __str__(self):
+        """Returns a printable square string made with `#`"""
+        string = ""
+        if self.size:
+            for _ in range(self.position[1]):
+                string += '\n'
+            for _ in range(self.size):
+                string += " " * self.position[0] + "#" * self.size + '\n'
+            return string[:-1]
+        return string
+    pass
